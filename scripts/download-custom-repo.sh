9fc8d4c08be993ffa70f4614c3457dde07728fca
Mike Battles <mbattles@redhat.com>
2018-11-09 17:27:10 -0500

    updating RPM downloading to support meta RPM

--- scripts/download-custom-repo.sh
+++ scripts/download-custom-repo.sh
@@ -4,16 +4,22 @@ set +u
 set +x
 
 RUN_DIR=$(realpath $( dirname $0 ))
+push $RUN_DIR
 
-OUTPUT_DIR="${RUN_DIR}/../output/Packages"
-COMP_PATH=$(realpath -L ${RUN_DIR}/../conf/bootable/comps.xml )
+OUTPUT_DIR="./output/Packages"
+mkdir -p ${OUTPUT_DIR}
+
+COMP_PATH=$(realpath -L ./conf/bootable/comps.xml )
 GROUP=${OUTPUT_DIR}/groups.xml
+
+# Compiling meta RPM to work around python-docker issue
+rm -rf /tmp/rpmbuild
+rpmbuild -bb contrib/python-docker.spec  --define "_rpmdir /tmp/rpmbuild"
+mv  /tmp/rpmbuild/x86_64/python-docker-0.0.1-1.x86_64.rpm ${OUTPUT_DIR}
 
 export OCP_VERSION=$(./contrib/bin/yaml_linux_amd64 read /opt/odie/config/ocp.yml ocp_version)
 
-# yumdownloader -x documented here: https://bugzilla.redhat.com/show_bug.cgi?id=1045871
-mkdir -p ${OUTPUT_DIR}
-yumdownloader -x \*i686 --archlist x86_64 --destdir ${OUTPUT_DIR} $(cat ${RUN_DIR}/../conf/*-rpms.txt| sort -u | envsubst)
+yumdownloader -x \*i686 --archlist x86_64 --destdir ${OUTPUT_DIR} $(cat ./conf/*-rpms.txt| sort -u | envsubst)
 
 cp -f $COMP_PATH ${GROUP}
 cd ${OUTPUT_DIR}/..
